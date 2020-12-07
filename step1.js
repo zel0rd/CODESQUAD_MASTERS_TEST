66648f50619f54b669d265211686cbb7038e4d9a
younggil <zelord.kwoun@gmail.com>
2020-12-07 13:32:27 +0900

    processing
    1. 입력 값을 분할
    2. 에러 처리
    3. count만큼 push함수 호출

--- step1.js
+++ step1.js
@@ -1,4 +1,6 @@
+const colors = require('colors');
 const readline = require("readline");
+const DIRECTIONS = ['L','R']
 
 function getInput() {
     // getInput - readline
@@ -22,8 +24,39 @@ function getInput() {
 }
 
 function processing(line){
-    push()
-    console.log(line)
+    let inputs = line.split(' ')
+
+    // error
+    if (inputs.length !== 3){
+        return console.log(`[INPUT ERROR]`.bgRed, `INVALID PARAMETERS => (text, count, direction)`.brightGreen)
+    }
+
+    let text = inputs[0]
+
+    // error
+    if (isNaN(parseInt(inputs[1]))){
+        return console.log(`[INPUT ERROR]`.bgRed, `ONLY NUMBERS CAN BE ENTERED IN COUNT`.brightGreen, inputs[1])
+    }
+    let count = parseInt(inputs[1])
+    let direction = inputs[2].toUpperCase()
+
+    // err
+    if (!DIRECTIONS.includes(direction)) {
+        return console.log(`[INPUT ERROR]`.bgRed, `UNABLE DIRECTION :`.brightGreen, direction)
+    }
+    
+    if (count < 0 ){
+        count*= -1
+        if(direction === DIRECTIONS[0]){
+            direction = DIRECTIONS[1]
+        } else {
+            direction = DIRECTIONS[0]
+        }
+    }
+    for(let i=0; i < count;i++){
+        text = push(text,direction)
+    }
+    console.log(text)
 }
 
 function push(text, direction){

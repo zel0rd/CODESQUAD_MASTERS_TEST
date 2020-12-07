0a4342203993bc5c405e3a9c0908d0c05c9d3e7f
younggil <zelord.kwoun@gmail.com>
2020-12-07 13:50:54 +0900

    skeleton code
    1. printArr
    2. getInput
    3. processing
    4. push

---
+++ step2.js
@@ -0,0 +1,29 @@
+const INIT_ARRAY = [['R','R','W'],['G','C','W'],['G','B','B']]
+
+function printArray(){
+
+}
+
+function getInput(){
+    // print INIT
+
+    // getInput
+    
+    // processing1
+    // push
+    // printArr
+
+    // processing2
+    // push
+    // printArr
+
+    // exit
+}
+
+function processing(){
+
+}
+
+function push(){
+
+}

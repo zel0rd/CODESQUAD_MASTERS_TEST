ceba0a9b0cd23015f87af503aadf8cd1f4d84f86
younggil <zelord.kwoun@gmail.com>
2020-12-07 12:45:52 +0900

    skeleton code
    getInput, process, push

---
+++ step1.js
@@ -0,0 +1,16 @@
+const readline = require("readline");
+
+function getInput() {
+    // getInput - readline
+    // processing - direction, text, count
+    // push - text, direction
+}
+
+function processing(){
+    push()
+}
+
+function push(){
+}
+
+getInput()

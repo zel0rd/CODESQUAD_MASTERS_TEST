a936e3afdca8560645976a2ced9ed9d573fdb490
younggil <zelord.kwoun@gmail.com>
2020-12-07 13:30:34 +0900

    getInput
    getInput until entered 'q' or 'quit'

--- step1.js
+++ step1.js
@@ -2,15 +2,31 @@ const readline = require("readline");
 
 function getInput() {
     // getInput - readline
-    // processing - direction, text, count
+    // process - direction, text, count
     // push - text, direction
+    let rl = readline.createInterface(process.stdin, process.stdout)
+    rl.setPrompt("> ")
+    rl.prompt();
+    rl.on('line', function(line) {
+        if (line === 'q' || line.toLowerCase()==='quit'){
+            rl.close()
+        } else {
+            processing(line)
+        }
+        rl.prompt()
+    })
+    .on('close', () => {
+        console.log("closed!!")
+        process.exit();
+    })
 }
 
-function processing(){
+function processing(line){
     push()
+    console.log(line)
 }
 
-function push(){
+function push(text, direction){
 }
 
 getInput()

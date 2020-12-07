10632f42e2c25b9b628cc3d207f891a463e9a3e3
younggil <zelord.kwoun@gmail.com>
2020-12-07 16:52:56 +0900

    skeleton

---
+++ step3.js
@@ -0,0 +1,32 @@
+function printArray(){
+    // 현재 큐브의 상태를 출력
+}
+
+function getInput(){
+    // 큐브를 조작할 순서를 받음
+}
+
+function parser(){
+    // F, R, U, B, L, D 이외에 숫자나 '(single quote)를 하나의 배열로 처리
+}
+
+function processing(){
+    // parser를 거친 input을 count 횟수만큼 push에 호출
+}
+
+function push(){
+    // 큐브를 조작
+    // 총 몇회를 조작했는지 기록
+}
+
+function timeMeasure(){
+    // 시작부터 끝날 때 까지의 시간을 기록
+}
+
+function mixCube(){
+    // 초기값에서 임의로 F,R,U,B,L,D를 사용한 문자열을 생성하여 수행
+}
+
+function completeCheck(){
+    // push를 수행할 때마다 모든 면이 같은 색상이 되었는지 확인
+}

51d05c0c1cb0c6b7ef3919f74f914ba166df7501
younggil <zelord.kwoun@gmail.com>
2020-12-10 11:38:22 +0900

    split function inputChecker, getPositiveCount

--- step1.js
+++ step1.js
@@ -20,28 +20,30 @@ function getInput() {
     })
 }
 
-function processing(line){
-    let inputs = line.split(' ')
-
-    // error
+function inputChecker(inputs){
+    // error1 : less than 3 parameters
     if (inputs.length !== 3){
         return console.log(`[INPUT ERROR]`.bgRed, `INVALID PARAMETERS => (text, count, direction)`.brightGreen)
     }
-
     let text = inputs[0]
 
-    // error
+    // error2 : count is not a number
     if (isNaN(parseInt(inputs[1]))){
         return console.log(`[INPUT ERROR]`.bgRed, `ONLY NUMBERS CAN BE ENTERED IN COUNT`.brightGreen, inputs[1])
     }
     let count = parseInt(inputs[1])
     let direction = inputs[2].toUpperCase()
 
-    // err
+    // error3 : wrong direction
     if (!DIRECTIONS.includes(direction)) {
         return console.log(`[INPUT ERROR]`.bgRed, `UNABLE DIRECTION :`.brightGreen, direction)
     }
-    
+
+    return "pass"
+}
+
+function getPositiveCount(count, direction){
+    // count가 음수일 경우, 양수로 변환하고 방향을 바꿔 줌
     if (count < 0 ){
         count*= -1
         if(direction === DIRECTIONS[0]){
@@ -50,10 +52,23 @@ function processing(line){
             direction = DIRECTIONS[0]
         }
     }
-    for(let i=0; i < count;i++){
-        text = push(text,direction)
+    return [count, direction]
+}
+
+function processing(line){
+    let inputs = line.split(' ')
+
+    if ( inputChecker(inputs) === "pass") {
+        let text = inputs[0]
+        let getPositive = getPositiveCount(parseInt(inputs[1]),inputs[2].toUpperCase())
+        let count = getPositive[0]
+        let direction = getPositive[1]
+
+        for(let i=0; i < count;i++){
+            text = push(text,direction)
+        }
+        console.log(text.blue)
     }
-    console.log(text.blue)
 }
 
 function push(text, direction){
